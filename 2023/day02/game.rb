16ab1afb38dc7b247b6cec6732a48f91e6a1becf
anazworth <98629900+anazworth@users.noreply.github.com>
2023-12-02 15:30:02 -0500

    Refactor day02

--- 2023/day02/game.rb
+++ 2023/day02/game.rb
@@ -3,71 +3,52 @@ class Game
     "red" => 12,
     "green" => 13,
     "blue" => 14
-  }
+  }.freeze
 
   def initialize(input)
     @input = input
   end
 
   def check
-    sum = 0
-
-    @input.each_line do |line|
-      game_id = line.match(/Game\s(\d*):\s/)[1].to_i
-
-      line.gsub!(/Game\s\d*:\s/, "")
-
-      sets = line.split(";")
-
-      game_is_possible = true
-
-      sets.each do |set|
-        games = set.split(",")
-        game_cubes = Hash.new(0)
-
-        games.each do |cube_str| # Save each color and value to a Hashmap
-          value, color = cube_str.strip.split(" ")
-          game_cubes[color] += value.to_i
-        end
-
-        game_cubes.each do |color, value| # Check if each color is under the threshold
-          game_is_possible = false unless value <= CUBES[color]
-        end
-      end
-
-      if game_is_possible
-        sum += game_id
-      end
+    @input.each_line.sum do |game|
+      game_id = game.match(/Game\s(\d*):\s/)[1].to_i
+      possible_game?(game.gsub!(/Game\s\d*:\s/, "").split(";")) ? game_id : 0
     end
-
-    return sum
   end
 
   def check_minimum
-    sum = 0
-    
-    @input.each_line do |line|
-      line.gsub!(/Game\s\d*:\s/, "")
-      max = Hash.new(0)
+    @input.each_line.map { |game| game_power(game) }.sum
+  end
 
-      sets = line.split(";")
+  private
+  def possible_game?(game)
+    game.each do |set|
+      game_cubes = Hash.new(0)
 
-      sets.each do |set|
-        games = set.split(",")
-        
-        games.each do |cube_str|
-          value, color = cube_str.strip.split(" ")
-          max[color] = [value.to_i, max[color]].max
-        end
+      # Save each color and value to a Hashmap
+      set.split(",").each do |cube_str|
+        value, color = cube_str.strip.split(" ")
+        game_cubes[color] += value.to_i
       end
 
-      power = 1
-      max.each do |_, value|
-        power *= value.to_i
+      # Check the game against the possible cubes
+      return false unless game_cubes.all? { |color, value| value <= CUBES[color] } 
+    end
+    return true
+  end
+
+  def game_power(game)
+    max = Hash.new(0)
+
+    game.gsub!(/Game\s\d*:\s/, "").split(";").each do |set|
+      set.split(",").each do |cube_str|
+        value, color = cube_str.strip.split(" ")
+        max[color] = [value.to_i, max[color]].max
       end
-      sum += power
     end
 
-    return sum
+    power = 1
+    max.each { |_, value| power *= value.to_i }
+    return power
   end
 end
