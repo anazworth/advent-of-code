019789554d45f8f64789b3bc3fcdc54a07ba392c
anazworth <98629900+anazworth@users.noreply.github.com>
2023-12-03 18:30:50 -0500

    Day03 complete and slight refactor

--- 2023/day03/schematic.rb
+++ 2023/day03/schematic.rb
@@ -1,33 +1,27 @@
 class Schematic
   def initialize(input)
-    @input = input
+    @input = input.split("\n")
   end
 
   def find_part_number
-    @input.split("\n").each_with_index.sum do |line, index|
+    @input.each_with_index.sum do |line, index|
       line_sum = 0
       part_nums = extract_by_pattern_with_index(line, /(\d+)/)
 
       part_nums.each do |num|
         line_length = line.length
-        left_index = [num[:left_index] - 1, 0].max
+        left_index = [num[:index] - 1, 0].max
         right_index = [left_index + num[:num].to_s.length + 1, line_length].min
         has_adj = false
 
         # Check left and right of part number
-        if line.slice(left_index..right_index).match(/[^0-9.]/)
-          has_adj = true
-        end
+        has_adj = true if line.slice(left_index..right_index).match(/[^0-9.]/)
 
         # Check line above part number
-        if index > 0 && @input.split("\n")[index - 1].slice(left_index..right_index).match(/[^0-9.]/)
-          has_adj = true
-        end
+        has_adj = true if index > 0 && @input[index - 1].slice(left_index..right_index).match(/[^0-9.]/)
 
         # Check line below part number
-        if index < @input.split("\n").length - 1 && @input.split("\n")[index + 1].slice(left_index..right_index).match(/[^0-9.]/)
-          has_adj = true
-        end
+        has_adj = true if index < @input.length - 1 && @input[index + 1].slice(left_index..right_index).match(/[^0-9.]/)
         
         line_sum += num[:num].to_i if has_adj
       end
@@ -36,26 +30,23 @@ class Schematic
   end
 
   def find_gear_ratio
-    @input.split("\n").each_with_index.sum do |line, index|
+    @input.each_with_index.sum do |line, index|
       line_sum = 0
       possible_gears = extract_by_pattern_with_index(line, /(\*)/)
-      pns_above = extract_by_pattern_with_index(@input.split("\n")[index - 1], /(\d+)/)
-      pns_below = extract_by_pattern_with_index(@input.split("\n")[index + 1], /(\d+)/) unless index >= @input.split("\n").length - 1
+      next 0 if possible_gears.length == 0 # Skip ahead if no gears are found
+      pns_above = extract_by_pattern_with_index(@input[index - 1], /(\d+)/) unless index == 0
+      pns_below = extract_by_pattern_with_index(@input[index + 1], /(\d+)/) unless index >= @input.length - 1
       pns_same_line = extract_by_pattern_with_index(line, /(\d+)/)
 
-      pns_in_context = []
-      pns_in_context.concat(pns_same_line)
+      pns_in_context = pns_same_line
       pns_in_context += pns_below if pns_below # The lines above/below might not exist
       pns_in_context += pns_above if pns_above
 
       possible_gears.each do |gear|
-        gear_index = gear[:left_index]
         adj_pns = []
 
         pns_in_context.each do |pn|
-          if (pn[:left_index] - 1..pn[:right_index] + 1).include?(gear_index)
-            adj_pns << pn[:num]
-          end
+          adj_pns << pn[:num] if (pn[:index] - 1..pn[:index] + pn[:num].length).include?(gear[:index])
         end
 
         line_sum += adj_pns.map(&:to_i).inject(:*) if adj_pns.length == 2
@@ -72,11 +63,10 @@ class Schematic
     line.scan(pattern) do |num_match|
       num = num_match.first
       left = Regexp.last_match.begin(0)
-      right = Regexp.last_match.end(0) - 1
 
       # Indexes have to be saved in a hashmap with each number to avoid
       # the edge case of the same pattern occuring twice on one line.
-      result << { num: num, left_index: left, right_index: right }
+      result << { num: num, index: left }
     end
     result
   end
